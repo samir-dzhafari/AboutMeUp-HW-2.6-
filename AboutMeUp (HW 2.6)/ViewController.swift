42b12380246f9766e39ac8fccbe5d0eec6d4f3b4
Samir Dzhafari <samirjafari2001@icloud.com>
2023-10-07 20:28:22 +0300

    Initial Commit

---
+++ AboutMeUp (HW 2.6)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeUp (HW 2.6)
+//
+//  Created by Самир Джафари on 07.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
